e492a2a152c632f0b5145738387ce3be68a27c3c
Yucked <yucked@protonmail.com>
2020-06-07 22:28:25 -0400

    Lets geddit

---
+++ js/github_api.js
@@ -0,0 +1,54 @@
+function fetch_repositories() {
+    var deferred = new $.Deferred();
+    var repositories = [];
+
+    $.get('https://api.github.com/users/Yucked/repos')
+        .done(function (data) {
+            $.each(data, function (_, value) {
+                var repository = {};
+                repository.name = value.name;
+                repository.description = value.description;
+                repository.url = value.html_url;
+                repository.forks = value.forks;
+                repository.stargazers = value.stargazers_count;
+
+                $.get(value.languages_url)
+                    .done(function (data) {
+                        repository.languages = data;
+                    })
+                    .fail(function () {
+                        repository.languages = value.language;
+                        console.log('Errored when trying to languages information.');
+                    });
+
+                repositories.push(repository);
+            });
+
+            deferred.resolve(repositories);
+        })
+        .fail(function () {
+            console.log('Errored when trying to get repositories.');
+            deferred.reject();
+        });
+
+    return deferred.promise();
+}
+
+function build_repository_html(repositories) {
+    repositories
+        .done(function (data) {
+            $.each(data, function (_, value) {
+                var template = $('#repository_template').clone();
+                template.find('#title').text(value.name);
+                template.find('#description').text(value.description);
+                template.find('#languages').text(value.languages);
+                template.find('#stars').text(value.stargazers);
+                template.find('#forks').text(value.forks);
+                template.find('#url').attr('href', value.url);
+                $('#repository_container').append(template.html());
+            });
+        })
+        .fail(function () {
+            $('#repository_error').removeClass('is-hidden');
+        });
+}
